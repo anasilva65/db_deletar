9f07676a257b0fbf684b5dfa3cede1368f4dd363
unknown <annadasilva89@hotmail.com>
2020-09-22 09:57:57 -0300

    banco de dados

---
+++ ana-aula22-09-DML.sql
@@ -0,0 +1,15 @@
+create database db_deletar;
+use db_deletar;
+
+create table consumidores (
+id int PRIMARY KEY AUTO_INCREMENT,
+nome varchar(30)
+);
+
+insert into consumidores (nome) values
+("Ana"),
+("Maria");
+
+select * from consumidores;
+delete from consumidores where id=1;
+select * from consumidores;
